90359cf166cc54fabbccc46be6ae76138658d9cc
Ryan Schlesinger <ryan@instanceinc.com>
2014-05-13 14:25:39 -0700

    Rewrite balance

--- lib/mbsy/resources/balance.rb
+++ lib/mbsy/resources/balance.rb
@@ -1,14 +1,26 @@
 module Mbsy
   class Balance < Base
     def self.update(mode, params={})
-      raise ArgumentError, "You must include :email" unless params[:email]
-      raise ArgumentError, "You must include :amount" unless params[:amount]
       case mode
       when :add
-        call('add', params)
+        add(params)
       when :deduct
-        call('deduct', params)
+        deduct(params)
       end
+    end
+
+    def self.add(params = {})
+      raise ArgumentError, "You must include :email" unless params[:email]
+      raise ArgumentError, "You must include :amount" unless params[:amount]
+
+      call('add', params)
+    end
+
+    def self.deduct(params = {})
+      raise ArgumentError, "You must include :email" unless params[:email]
+      raise ArgumentError, "You must include :amount" unless params[:amount]
+
+      call('deduct', params)
     end
   end
 end
