31f8a0f1e8823d1ff6b375cbf1747b0ca53276a2
Ryan Schlesinger <ryan@instanceinc.com>
2014-05-13 13:54:59 -0700

    Remove whitespace from mbsy_spec

--- spec/mbsy_spec.rb
+++ spec/mbsy_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe Mbsy do
   context 'configuration setup' do
-  
     it "builds the url correctly" do
       expect {
         Mbsy.configure do |c|
@@ -11,6 +10,5 @@ describe Mbsy do
         end
       }.to change { Mbsy.site_uri.to_s }.to("https://getambassador.com/api/v2/tester/API-123/json/")
     end
-
   end
 end
